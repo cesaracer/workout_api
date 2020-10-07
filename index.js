f15eaab1a6cb4c16b7927151ae5980e3d78b1599
cesaracer <38962217+cesaracer@users.noreply.github.com>
2020-10-06 18:42:50 -0600

    added comments

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
+//connecting to db
 mongoose.connect("myconnectionstring", { useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection
 
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to database'))
 
+//initializing router
 const router = require('./routes/main')
 
 app.use(express.json())
 
+//defining router path
 app.use('/routines', router)
 
 app.listen(5000, () => console.log('Server started'))
